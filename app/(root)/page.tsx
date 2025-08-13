66e664c8bfee4a5adb45014e499d06322defb8fe
Ben <ben.allen.02.11@gmail.com>
2025-08-13 03:34:13 -0400

    Completed basic Navbar, and Header for the site
    Created a consistent Navbar and Header to appear throughout the site.
    
    Added in common use Icons and test images for purposes of placeholders in the project, to potentially be updated later.
    
    Created the initial routing folders/paths for the websites URL.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className='wrapper page'>
+      <Header title="All Videos" subHeader='Public Library'/>
+      <h1 className="text-2xl font-karla">
+        Welcome to Scene Drop
+      </h1>
+    </main>
+  )
+}
+
+export default Page
